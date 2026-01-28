9f596fd32ec3db969347ee399e46f26b0795f501
JIA-WEI-LI <magicsoldier19@gmail.com>
2026-01-28 09:24:18 +0800

    refactor(core): streamline StateColorResolver and decouple component naming (Issue #12)

--- src/pylunix/common/factories.py
+++ src/pylunix/common/factories.py
@@ -9,23 +9,16 @@ class StateColorResolver:
         is_pressed = getattr(widget, "isPressed", False)
         is_hover = getattr(widget, "isHover", False)
 
-        is_checked = False
-        if hasattr(widget, "isChecked"):
-            is_checked = widget.isChecked()
+        if not is_enabled: 
+            state = "Disabled"
+        elif is_pressed: 
+            state = "Pressed"
+        elif is_hover: 
+            state = "PointerOver"
+        else: 
+            state = "" # Normal
 
-        state_suffix = ""
+        name = f"{component}{attribute}{state}"
         
-        if is_checked:
-            state_suffix = "Checked"
-            if not is_enabled: state_suffix += "Disabled"
-            elif is_pressed: state_suffix += "Pressed"
-            elif is_hover: state_suffix += "PointerOver"
-        else:
-            if not is_enabled: state_suffix = "Disabled"
-            elif is_pressed: state_suffix = "Pressed"
-            elif is_hover: state_suffix = "PointerOver"
-            else: state_suffix = "" # Normal state
-
-        name = f"{component}{attribute}{state_suffix}"
         stylesheet_category = getattr(PyLunixStyleSheet, theme_type)
         return stylesheet_category.get_value(name)
